f4adffe7073ac01243797157e3041b28dfd0139d
François Voron <fvoron@gmail.com>
2024-02-12 18:00:27 +0100

    Add docstrings

---
+++ docs/reference/pwdlib.exceptions.md
@@ -0,0 +1,6 @@
+# Reference
+
+::: pwdlib.exceptions
+    options:
+      show_root_heading: false
+      show_source: false
--- pwdlib/_hash.py
+++ pwdlib/_hash.py
@@ -5,7 +5,18 @@ from .hashers import HasherProtocol
 
 
 class PasswordHash:
+    """
+    Represents a password hashing utility.
+    """
+
     def __init__(self, hashers: typing.Sequence[HasherProtocol]) -> None:
+        """
+        Args:
+            hashers: A sequence of hashers to be used for password hashing.
+
+        Raises:
+            AssertionError: If no hashers are specified.
+        """
         assert len(hashers) > 0, "You must specify at least one hasher."
         self.hashers = hashers
         self.current_hasher = hashers[0]
@@ -16,11 +27,34 @@ class PasswordHash:
         *,
         salt: typing.Union[bytes, None] = None,
     ) -> str:
+        """
+        Hashes a password using the current hasher.
+
+        Args:
+            password: The password to be hashed.
+            salt: The salt to be used for hashing. Defaults to None.
+
+        Returns:
+            The hashed password.
+        """
         return self.current_hasher.hash(password, salt=salt)
 
     def verify(
         self, hash: typing.Union[str, bytes], password: typing.Union[str, bytes]
     ) -> bool:
+        """
+        Verifies if a password matches a given hash.
+
+        Args:
+            hash: The hash to be verified.
+            password: The password to be checked.
+
+        Returns:
+            True if the password matches the hash, False otherwise.
+
+        Raises:
+            exceptions.UnknownHashError: If the hash is not recognized by any of the hashers.
+        """
         for hasher in self.hashers:
             if hasher.identify(hash):
                 return hasher.verify(hash, password)
@@ -29,6 +63,20 @@ class PasswordHash:
     def verify_and_update(
         self, hash: typing.Union[str, bytes], password: typing.Union[str, bytes]
     ) -> typing.Tuple[bool, typing.Union[str, None]]:
+        """
+        Verifies if a password matches a given hash and updates the hash if necessary.
+
+        Args:
+            hash: The hash to be verified.
+            password: The password to be checked.
+
+        Returns:
+            A tuple containing a boolean indicating if the password matches the hash,
+                and an updated hash if the current hasher or the hash itself needs to be updated.
+
+        Raises:
+            exceptions.UnknownHashError: If the hash is not recognized by any of the hashers.
+        """
         for hasher in self.hashers:
             if hasher.identify(hash):
                 if not hasher.verify(hash, password):
