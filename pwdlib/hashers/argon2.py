f2ef29e0b4cbadccd56f991658d42b7a6c01faf5
Eric Westfall <eawestfall@gmail.com>
2025-10-22 18:32:41 -0700

    fix: add support for all valid Argon2 type variants
    This commit changes the behavior of the Argon2Hasher identify method to
    correctly identify and signal support for all valid Argon2 type variants
    which at the time of writing include 'argon2id', 'argon2d', and
    'argon2i'.
    
    This commit also enhances the hash validation mechanism to move from
    only checking the hash type against a hard-coded string to validating
    the complete hash against a comprehensive regex pattern and verifying
    the type variant against the `argon2-cffi` backend library to ensure the
    user-supplied hash is a valid and well-formed Argon2 hash.
    
    Resolves #21.

--- pwdlib/hashers/argon2.py
+++ pwdlib/hashers/argon2.py
@@ -1,6 +1,10 @@
+import re
+from typing import ClassVar
+
 try:
     import argon2.exceptions
     from argon2 import PasswordHasher
+    from argon2._utils import _NAME_TO_TYPE
 except ImportError as e:
     from ..exceptions import HasherNotAvailable
 
@@ -8,8 +12,20 @@ except ImportError as e:
 
 from .base import HasherProtocol, ensure_str
 
+# Pattern for identifying and validating an Argon2 encoded hash, covering all currently
+# supported type variants (i.e., `id`, `i`, `d`). Pattern uses deterministic matching,
+# explicit anchors, and a non-greedy terminal quantifier to ensure linear run time
+# relative to input hash length and resilience against catastrophic backtracking attacks.
+ARGON2_ENCODED_HASH_REGEX: re.Pattern = re.compile(
+    r"^\$argon2(?P<type>id|i|d)\$(?:v=(?P<version>\d+)\$)?"
+    r"m=(?P<memory_cost>\d+),t=(?P<time_cost>\d+),p=(?P<parallelism>\d+)"
+    r"(?:\$(?P<salt>[^$]+)(?:\$(?P<digest>.+?))?)?$"
+)
+
 
 class Argon2Hasher(HasherProtocol):
+    name: ClassVar[str] = "argon2"
+
     def __init__(
         self,
         time_cost: int = argon2.DEFAULT_TIME_COST,
@@ -40,7 +56,38 @@ class Argon2Hasher(HasherProtocol):
 
     @classmethod
     def identify(cls, hash: str | bytes) -> bool:
-        return ensure_str(hash).startswith("$argon2id$")
+        """
+        Returns True if the supplied hash is a supported Argon2 hash.
+
+        Checks if the supplied hash value is a valid and well-formed Argon2 encoded
+        hash, safely converting bytes to a string if required.
+
+        Args:
+            hash (`str` | `bytes`): The hash value that should be evaluated.
+
+        Returns:
+            bool: Returns `True` if the supplied hash completely matches the Argon2
+                regex pattern defined in `_ARGON2_ENCODED_HASH_REGEX` and presents a
+                supported Argon2 variant, otherwise `False`. Any decode or type errors
+                will also return `False`.
+        """
+        try:
+            hash_str: str = ensure_str(hash)
+            # This type guard is only necessary until the ensure_str function enforces
+            # a stronger guarantee on its return type. At present, ensure_str will
+            # simply reflect any type it receives that isn't a bytes object.
+            if not isinstance(hash_str, str):
+                return False
+        except UnicodeDecodeError:
+            return False  # Unable to decode the supplied hash.
+
+        match = ARGON2_ENCODED_HASH_REGEX.fullmatch(hash_str)
+        if match is None:
+            return False  # Hash is not a valid or well-formed Argon2 hash.
+
+        variant: str = match.group("type")  # ('id', 'd', 'i')
+        type_name = f"{cls.name}{variant}"  # ('argon2id', 'argon2d', 'argon2i')
+        return type_name in _NAME_TO_TYPE
 
     def hash(self, password: str | bytes, *, salt: bytes | None = None) -> str:
         return self._hasher.hash(password, salt=salt)
