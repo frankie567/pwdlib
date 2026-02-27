64f1ba871286daea8b6c8d70ee21d2773035bfbf
Max Marrone <max@marrone.nyc>
2026-02-27 13:52:39 -0500

    Clarify salt=None.

--- pwdlib/_hash.py
+++ pwdlib/_hash.py
@@ -45,7 +45,7 @@ class PasswordHash:
 
         Args:
             password: The password to be hashed.
-            salt: The salt to be used for hashing. Defaults to None.
+            salt: The salt to be used for hashing. If set to `None`, a random salt is used.
 
         Returns:
             The hashed password.
